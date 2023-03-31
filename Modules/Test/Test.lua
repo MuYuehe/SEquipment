e51cd553ea1a8833e546c80e8fc347b402face25
MuYueHe <lpengcheng0220@gmail.com>
2023-03-31 14:54:49 +0800

    删除main中的测试代码

--- Modules/Test/Test.lua
+++ Modules/Test/Test.lua
@@ -10,20 +10,20 @@ Scorpio "SEquipment.Test" ""
 --     -- C_Container.GetContainerItemInfo(bagID, self:GetID()) --10.0加入的新api,与GetContainerItemInfo不同
 -- end
 
-__SystemEvent__ "NAME_PLATE_UNIT_ADDED" "FORBIDDEN_NAME_PLATE_UNIT_ADDED"
-function EVENT_NAME_PLATE_UNIT_ADDED(unitToken)
-    local namePlateFrameBase = C_NamePlate.GetNamePlateForUnit(unitToken, issecure());
-    if not namePlateFrameBase.f then
-        namePlateFrameBase.f = Frame(namePlateFrameBase:GetName() .. "TestClassIcon", namePlateFrameBase)
-        namePlateFrameBase.f:SetPoint("CENTER",0, 40)
-        namePlateFrameBase.f:SetSize(40, 40)
+-- __SystemEvent__ "NAME_PLATE_UNIT_ADDED" "FORBIDDEN_NAME_PLATE_UNIT_ADDED"
+-- function EVENT_NAME_PLATE_UNIT_ADDED(unitToken)
+--     local namePlateFrameBase = C_NamePlate.GetNamePlateForUnit(unitToken, issecure());
+--     if not namePlateFrameBase.f then
+--         namePlateFrameBase.f = Frame(namePlateFrameBase:GetName() .. "TestClassIcon", namePlateFrameBase)
+--         namePlateFrameBase.f:SetPoint("CENTER",0, 40)
+--         namePlateFrameBase.f:SetSize(40, 40)
 
-        namePlateFrameBase.tex = namePlateFrameBase.f:CreateTexture()
-        namePlateFrameBase.tex:SetAllPoints(namePlateFrameBase.f)
-        -- namePlateFrameBase.tex:SetTexture("Interface/GLUES/CHARACTERCREATE/UI-CHARACTERCREATE-CLASSES")
-    end
-    local className, class = UnitClass(unitToken)
+--         namePlateFrameBase.tex = namePlateFrameBase.f:CreateTexture()
+--         namePlateFrameBase.tex:SetAllPoints(namePlateFrameBase.f)
+--         -- namePlateFrameBase.tex:SetTexture("Interface/GLUES/CHARACTERCREATE/UI-CHARACTERCREATE-CLASSES")
+--     end
+--     local className, class = UnitClass(unitToken)
 
-    namePlateFrameBase.tex:SetMask("Interface/COMMON/Indicator-Gray")
-    namePlateFrameBase.tex:SetTexture("Interface/ICONS/ClassIcon_" .. class)
-end
+--     namePlateFrameBase.tex:SetMask("Interface/COMMON/Indicator-Gray")
+--     namePlateFrameBase.tex:SetTexture("Interface/ICONS/ClassIcon_" .. class)
+-- end
